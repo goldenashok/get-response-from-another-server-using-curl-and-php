c6273d8c954586745d10c93b50931efb70024521
Ashok Kumar D <goldenashok@users.noreply.github.com>
2024-01-12 16:41:07 +0530

    Create getdata.php

---
+++ getdata.php
@@ -0,0 +1,14 @@
+<?php
+	$apiurl = '<Domain>'.'/test1.php';
+	$ch = curl_init();
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL, $apiurl);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  $response = curl_exec($ch);
+  $err = curl_error($ch);
+  curl_close($ch);
+	echo $response;
+?>
