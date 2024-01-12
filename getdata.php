d7b9cea2620ada11cd5c039b1f466b0488e184d9
Ashok Kumar D <goldenashok@users.noreply.github.com>
2024-01-12 16:44:44 +0530

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -1,14 +1,14 @@
 <?php
 	$apiurl = '<Domain>'.'/getdata-response.php';
 	$ch = curl_init();
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_URL, $apiurl);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  $response = curl_exec($ch);
-  $err = curl_error($ch);
-  curl_close($ch);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $apiurl);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
 	echo $response;
 ?>
