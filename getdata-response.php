d921b5ad08f0ef5df55aa073bd5bbbe0ce14e99b
Ashok Kumar D <goldenashok@users.noreply.github.com>
2024-01-12 16:43:03 +0530

    Create getdata-response.php

---
+++ getdata-response.php
@@ -0,0 +1,14 @@
+<?php
+	$myval = "<table>";
+	$myval .= "<tr><th>Name</th>";
+	$myval .= "<th>Mobile</th>";
+	$myval .= "<th>Status</th></tr>";
+	$myval .= "<tr><td>Ashok Kumar</td>";
+	$myval .= "<td>9952472946</td>";
+	$myval .= "<td>Active</td></tr>";
+	$myval .= "<tr><td>Kumar</td>";
+	$myval .= "<td>9952472946</td>";
+	$myval .= "<td>In Active</td></tr>";
+	$myval .= "</table>";
+	echo $myval;
+?>
